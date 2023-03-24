6d2e32bd08370ea8559bd172d18a2ec7eabf0743
Miles Baffour <mbaffour24@cmc.edu>
2023-03-23 22:57:57 -0700

    fixing BinaryTree.py

--- containers/BinaryTree.py
+++ containers/BinaryTree.py
@@ -127,7 +127,7 @@ class BinaryTree():
         elif traversal_type == 'postorder':
             return self.postorder(self.root, '')
         else:
-            raise ValueError('Traversal type ' + str(traversal_type) +' is not supported.')
+            raise ValueError('Traversal type ' + str(traversal_type) + ' is not supported.')
 
     def preorder(self, start, traversal):
         '''
